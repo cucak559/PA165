43fdaf86f42ab24f94ded9794f3b383ca676be1e
Michal Čaniga <michalcaniga559@gmail.com>
2020-03-20 18:06:56 +0100

    seminar04

--- eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
+++ eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
@@ -1,12 +1,69 @@
 package cz.fi.muni.pa165.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Objects;
 
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private Color color;
+
+    private java.util.Date addedDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public java.util.Date getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(java.util.Date addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+                Objects.equals(name, product.name) &&
+                color == product.color &&
+                Objects.equals(addedDate, product.addedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color, addedDate);
+    }
 }
